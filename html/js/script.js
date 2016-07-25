c49e5cd3c2010fcdf800d32b570c1feb0fbe1cbe
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2016-07-25 20:04:18 +0200

    Add minor fix

--- html/js/script.js
+++ html/js/script.js
@@ -290,6 +290,7 @@ function ajax(f,obj){
 						}
 
 						$('.eachsignature').on('click', function (e) {
+							('#contract-viewer').off('click');
 							if($(this).hasClass('completed'))
 								return;
 							
