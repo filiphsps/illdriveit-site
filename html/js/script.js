a09cfce7ac62f6eff8fa5370a551e32619fbb584
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2016-07-26 04:12:31 +0200

    Add TX & GA. Fix minor issues.

--- html/js/script.js
+++ html/js/script.js
@@ -291,6 +291,8 @@ function ajax(f,obj){
 					var res = JSON.parse(data.responseText);
 					if(res.Success){
 						$('#signaturebuttons').html('');
+						if(user.state.length > 2)
+							user.state = abbrState(user.state);
 
 						//Render MBPI
 						ContractManager.RequestContract('MBPI', '#contract-viewer', user, function () {
@@ -330,6 +332,7 @@ function ajax(f,obj){
 								);
 							}
 
+							alert(user.state);
 							$('.eachsignature').on('click', function (e) {
 								if($(this).hasClass('completed'))
 									return;
@@ -548,4 +551,68 @@ function get_cent(num){
 	}else{
 		return num;
 	}
+}
+
+//From https://gist.github.com/CalebGrove/c285a9510948b633aa47
+function abbrState (input, to) {
+    var states = [
+        ['Arizona', 'AZ'],
+        ['Alabama', 'AL'],
+        ['Alaska', 'AK'],
+        ['Arizona', 'AZ'],
+        ['Arkansas', 'AR'],
+        ['California', 'CA'],
+        ['Colorado', 'CO'],
+        ['Connecticut', 'CT'],
+        ['Delaware', 'DE'],
+        ['Florida', 'FL'],
+        ['Georgia', 'GA'],
+        ['Hawaii', 'HI'],
+        ['Idaho', 'ID'],
+        ['Illinois', 'IL'],
+        ['Indiana', 'IN'],
+        ['Iowa', 'IA'],
+        ['Kansas', 'KS'],
+        ['Kentucky', 'KY'],
+        ['Kentucky', 'KY'],
+        ['Louisiana', 'LA'],
+        ['Maine', 'ME'],
+        ['Maryland', 'MD'],
+        ['Massachusetts', 'MA'],
+        ['Michigan', 'MI'],
+        ['Minnesota', 'MN'],
+        ['Mississippi', 'MS'],
+        ['Missouri', 'MO'],
+        ['Montana', 'MT'],
+        ['Nebraska', 'NE'],
+        ['Nevada', 'NV'],
+        ['New Hampshire', 'NH'],
+        ['New Jersey', 'NJ'],
+        ['New Mexico', 'NM'],
+        ['New York', 'NY'],
+        ['North Carolina', 'NC'],
+        ['North Dakota', 'ND'],
+        ['Ohio', 'OH'],
+        ['Oklahoma', 'OK'],
+        ['Oregon', 'OR'],
+        ['Pennsylvania', 'PA'],
+        ['Rhode Island', 'RI'],
+        ['South Carolina', 'SC'],
+        ['South Dakota', 'SD'],
+        ['Tennessee', 'TN'],
+        ['Texas', 'TX'],
+        ['Utah', 'UT'],
+        ['Vermont', 'VT'],
+        ['Virginia', 'VA'],
+        ['Washington', 'WA'],
+        ['West Virginia', 'WV'],
+        ['Wisconsin', 'WI'],
+        ['Wyoming', 'WY'],
+    ];
+	input = input.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+	for(i = 0; i < states.length; i++){
+		if(states[i][0] == input){
+			return(states[i][1]);
+		}
+	}    
 }
