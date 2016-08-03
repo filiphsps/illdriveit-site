cf2a196101a0944ffc791548dc21f90598d27c74
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2016-08-03 22:16:35 +0200

    Handle missing receipt.

--- html/receipt/app.js
+++ html/receipt/app.js
@@ -6,19 +6,25 @@ $.ajax({
     success: function(res) {
         console.log(res);
 
+        if(res.status !== 200)
+            return $('main').html('<h1 class="error">Error!<br/>Cannot find the receipt #' + receipt_id + '<h1>');
+
         $('.name').text(res.first_name + ' ' + res.last_name);
         $('.address').text(res.address);
         $('.location').text(res.city + ', ' + res.state + ' ' + res.zip);
         $('.vin').text(res.vin);
 
         $('.years').text(res.years);
+        $('.months').text(res.months);
         $('.miles').text(res.miles);
 
         $('.payment-down').text('$' + res.down);
         $('.payment-month').text('$' + res.month);
+        $('.cc1').text('0000');
+        $('.cc2').text('0000');
         $('.total-sum').text('$' + res.total);
     },
     error: function(err) {
-        //$('main').html('<h1 class="error">Error!<br/>Cannot find the receipt #' + receipt_id + '<h1>');
+        $('main').html('<h1 class="error">Error!<br/>Cannot find the receipt #' + receipt_id + '<h1>');
     }
 });
