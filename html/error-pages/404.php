32aab50698bdc42d4be97a20967043e7ec6a34d1
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2016-08-13 02:58:52 +0200

    Add 404 page.

---
+++ html/error-pages/404.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>HOME - illdrive.it</title>
+		<meta charset="UTF-8">
+		<meta content="width=device-width, initial-scale=1, maximum-scale=1" name="viewport"/>
+		<link href="/assets/css/bootstrap.min.css" rel="stylesheet"/>
+		<link href="/assets/css/style.css" rel="stylesheet" type="text/css"/>
+		<link href="/assets/css/style2.css" rel="stylesheet" type="text/css"/>
+		<link href="https://www.illdrive.it/assets/img/logo.png" rel="icon" type="image/png"/>
+	</head>
+	<body>
+		<?php include($_SERVER['DOCUMENT_ROOT'] . '/templates/header.php');?>
+
+		<section id="warranty">
+			<div class="container-fluid contactpage">
+				<div class="col-md-12 col-sm-12 col-xs-12">
+					<h1 class="text-center">Error 404</h1>
+					<h2 class="text-center">Page not found :(</h2>
+				</div>
+			</div>
+		</section>
+
+		<?php include($_SERVER['DOCUMENT_ROOT'] . '/templates/footer.php');?>
+	</body>
+</html>
