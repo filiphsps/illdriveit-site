623ef780bcd5449b63600e1982d7f98161be63f1
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2016-08-16 01:35:17 +0200

    Add ref on mobile.

--- forcefield/index.php
+++ forcefield/index.php
@@ -316,11 +316,11 @@
                         </div>
                         <div class="benefit text-center">
                             <figure>
-                                <div><img src="../assets/img/uber.png" alt="">
+                                <div><img class="referral-logo-covers" src="../assets/img/uber.png" alt="">
                             </figure>
                             <h2>
                         THE <b>FIRST</b> PLAN THAT</br>
-                        COVERS RIDESHARE DRIVERS
+                        COVERS <span class="referral-tagline">RIDESHARE</span> DRIVERS
                     </h2>
                             </div>
                             <div class="benefit text-center">
