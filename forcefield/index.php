d3a61095f0b884fe5f7944ce5bd4291b9dad421c
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2016-08-17 21:40:30 +0200

    Add fb pixel.

--- forcefield/index.php
+++ forcefield/index.php
@@ -7,6 +7,20 @@
 
     <body>
         <?php include($_SERVER['DOCUMENT_ROOT'] . '/templates/header.php');?>
+        <!-- Facebook Pixel Code -->
+        <script>
+        !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+        n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+        document,'script','https://connect.facebook.net/en_US/fbevents.js');
+
+        fbq('init', '650578218425295');
+        fbq('track', "PageView");</script>
+        <noscript><img height="1" width="1" style="display:none"
+        src="https://www.facebook.com/tr?id=650578218425295&ev=PageView&noscript=1"
+        /></noscript>
+        <!-- End Facebook Pixel Code -->
 
         <section id="warranty" class="display-table section-screen-height section-height-header">
             <div class="vertical-align-screen-height">
