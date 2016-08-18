194909188f65026a06412b73f3f93ca4cc36ed4e
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2016-08-18 02:26:22 +0200

    Prepare gulp for ci.

--- gulpfile.js
+++ gulpfile.js
@@ -3,30 +3,52 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     sourcemaps = require('gulp-sourcemaps'),
     autoprefixer = require('gulp-autoprefixer'),
+    pug = require('gulp-pug');
+    browserSync = require('browser-sync').create(),
 
-    input = './scss/**/*.scss',
     output = './www/';
 
-var sassOptions = {
-  errLogToConsole: true,
-  outputStyle: 'expanded'
-};
-
 gulp.task('scss', function () {
-  return gulp
-    .src(input)
-    .pipe(sourcemaps.init())
-    .pipe(sass(sassOptions).on('error', sass.logError))
-    .pipe(autoprefixer())
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest(output + 'css'))
-    .pipe(rename({suffix: '.min'}));
+    return gulp
+        .src('./scss/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            errLogToConsole: true,
+            outputStyle: 'expanded'
+        }).on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(output + 'css'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
 });
 
-gulp.task('watch', function() {
-  return gulp
-    .watch(input, ['scss'])
-    .on('change', function(event) {
-      console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-    });
+gulp.task('views', function buildHTML() {
+    return gulp
+        .src('./views/*.pug')
+        .pipe(pug({
+            // Your options in here. 
+        }))
+        .pipe(gulp.dest(output))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
+
+gulp.task('watch', ['serve'], function() {
+    gulp.watch('./scss/**/*.scss', ['scss']);
+    gulp.watch('./views/**/*.pug', ['views'])
+});
+gulp.task('serve', function() {
+    browserSync.init({
+        server: {
+            baseDir: 'www'
+        },
+    })
+})
+
+gulp.task('default', ['scss', 'views'], function() {
+    
 });
